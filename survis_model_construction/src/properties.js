25cb1e29931d67d52dfe6a77c44a8b4cebe1c508
poemlove-123 <912567741@qq.com>
2025-05-16 01:20:43 +0100

    Add files via upload

---
+++ survis_model_construction/src/properties.js
@@ -0,0 +1,16 @@
+var properties = {
+  title: "SurVis – Model Construction Games",
+  yearAttribute: "year",
+  attributes: {
+    authors: {
+      title: "Authors",
+      isList: true
+    },
+    venue: {
+      title: "Venue"
+    },
+    doi: {
+      title: "DOI"
+    }
+  }
+};
